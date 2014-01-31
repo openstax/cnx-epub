482cbe39a0d35b44d160b25c1bb3f4a5259d5b88
Michael Mulich <michael.mulich@gmail.com>
2014-01-30 19:50:07 -0500

    Adds document parsing code to extract metadata and create navigation trees.

--- cnxepub.py
+++ cnxepub.py
@@ -22,6 +22,13 @@ EPUB_OPF_NAMESPACES = {
     'opf': "http://www.idpf.org/2007/opf",
     'dc': "http://purl.org/dc/elements/1.1/",
     }
+HTML_DOCUMENT_NAMESPACES = {
+    'xhtml': "http://www.w3.org/1999/xhtml",
+    'epub': "http://www.idpf.org/2007/ops",
+    }
+
+DOCUMENT_MIMETYPE = 'application/vnd.org.cnx.module'
+COLLECTION_MIMETYPE = 'application/vnd.org.cnx.collection'
 
 
 class EPUB(MutableSequence):
@@ -158,3 +165,103 @@ class Item(object):
         with open(filepath, 'rb') as fb:
             data = io.BytesIO(fb.read())
         return cls(name, data, **kwargs)
+
+
+def epub_to_mapping(epub):
+    package_mappings = []
+    for package in epub:
+        documents, tree, metadata = _parse_epub_navigation(package.navigation)
+        item_mappings = {
+            package.navigation.name: {
+                'type': COLLECTION_MIMETYPE,
+                'metadata': metadata,
+                'tree': tree,
+                },
+            }
+        for doc_name in documents:
+            doc = package.grab_by_name(doc_name)
+            doc_etree = etree.fromstring(doc.data.read())
+            item_mappings[doc.name] = {
+                'type': DOCUMENT_MIMETYPE,
+                'metadata': _parse_document_metadata(doc_etree),
+                'document': io.BytesIO(etree.tostring(doc_etree)),
+                }
+        package_mappings.append(item_mappings)
+    return package_mappings
+
+
+def _parse_document_metadata(root):
+    """Given an etree element, parse out the metadata to a dictionary.
+    This only parses the data. It does not validate it.
+    """
+    xpath = lambda x: root.xpath(x, namespaces=HTML_DOCUMENT_NAMESPACES)
+    metadata = {
+        'title': xpath('//xhtml:title/text()')[0],
+        'language': xpath('//xhtml:meta[@itemprop="inLanguage"]/@content')[0],
+        'abstract': xpath('//xhtml:meta[@itemprop="description"]/@content')[0],
+        'subjects': [x for x in xpath('//xhtml:meta[@itemprop="about"]/@content')],
+        'keywords': [x for x in xpath('//xhtml:meta[@itemprop="keywords"]/@content')],
+        ##'license': xpath('//xhtml:meta[@property="dc:license"]/@content')[0],
+        'license_url': xpath('//xhtml:link[@rel="lrmi:useRightsUrl"]/@href')[0],
+        'publishers': [x for x in xpath('//xhtml:meta[@itemprop="publisher"]/@content')],
+        'editors': [x for x in xpath('//xhtml:meta[@itemprop="editor"]/@content')],
+        'illustrators': [x for x in xpath('//xhtml:meta[@itemprop="illustrator"]/@content')],
+        'translators': [x for x in xpath('//xhtml:meta[@itemprop="translator"]/@content')],
+        'copyright_holders': [x for x in xpath('//xhtml:meta[@itemprop="copyrightHolder"]/@content')],
+        'created': [x for x in xpath('//xhtml:meta[@itemprop="dateCreated"]/@content')][0],
+        'revised': [x for x in xpath('//xhtml:meta[@itemprop="dateModified"]/@content')][0],
+        }
+    unordered = []
+    for elm in xpath('//xhtml:meta[@itemprop="author"]'):
+        elm_id = elm.get('id', None)
+        uid = elm.get('content')
+        order = None
+        if elm_id is not None:
+            try:
+                order = xpath('//xhtml:meta[@refines="{}" and @property="display-seq"]/@content'.format(elm_id.lstrip('#')))[0]
+            except IndexError:
+                pass  # Check for refinement failed, maintain None value.
+        unordered.append((order, uid,))
+    ordered = sorted(unordered,
+                     cmp=lambda x,y: x is None or y is None and -1 or cmp(x,y),
+                     key=lambda x: x[0])
+    metadata['authors'] = [x[1] for x in ordered]
+    return metadata
+
+
+def _nav_to_tree(root):
+    """Given an etree containing a navigation document structure
+    rooted from the 'nav' element, parse to a tree:
+    {'id': <id>|'subcol', 'title': <title>, 'contents': [<tree>, ...]}
+    """
+    for li in root.xpath('xhtml:ol/xhtml:li', namespaces=HTML_DOCUMENT_NAMESPACES):
+        classes = li.get('class', '').split()
+        is_subtree = bool([e for e in li.getchildren()
+                           if e.tag[e.tag.find('}')+1:] == 'ol'])
+        if is_subtree:
+            # It's a sub-tree and have a 'span' and 'ol'.
+            yield {'id': 'subcol',  # Special id...
+                   'title': li.xpath('xhtml:span/text()',
+                                     namespaces=HTML_DOCUMENT_NAMESPACES)[0],
+                   'contents': [x for x in _nav_to_tree(li)],
+                   }
+        else:
+            # It's a node and should only have an li.
+            a = li.xpath('xhtml:a', namespaces=HTML_DOCUMENT_NAMESPACES)[0]
+            yield {'id': a.get('href'), 'title': a.text}
+    raise StopIteration()
+
+def _parse_epub_navigation(nav):
+    """Parse the navigation items to a name list and tree,
+    both of which are using item names as values.
+    Returns the name list, tree and document metadaa.
+    """
+    tree = []
+    nav_doc = etree.fromstring(nav.data.read())
+    nav_metadata = _parse_document_metadata(nav_doc)
+    doc_names = [href for href in nav_doc.xpath('//xhtml:a/@href', namespaces=HTML_DOCUMENT_NAMESPACES)]
+    tree = {'id': nav.name,
+            'title': nav_metadata['title'],
+            'contents': [x for x in _nav_to_tree(nav_doc.xpath('//xhtml:nav', namespaces=HTML_DOCUMENT_NAMESPACES)[0])]
+            }
+    return doc_names, tree, nav_metadata
--- tests.py
+++ tests.py
@@ -9,6 +9,7 @@ import os
 import tempfile
 import unittest
 
+from lxml import etree
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -35,6 +36,7 @@ SINGLE_OPF_METADATA = {
     # <meta property="cc:attributionURL">http://cnx.org/content</meta>
     'base_attribution_url': "http://cnx.org/content",
     }
+TEST_EPUB_FILEPATH = SINGLE_OPF_FILEPATH
 
 
 class EPUBTestCase(unittest.TestCase):
@@ -59,12 +61,121 @@ class EPUBTestCase(unittest.TestCase):
         self.assertEqual(package.metadata, SINGLE_OPF_METADATA)
 
         # Check the navigation order
-        expected_nav_document = package.grab_by_name("9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6.xhtml")
+        expected_nav_document = package.grab_by_name(
+                "9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6.xhtml")
         self.assertTrue(expected_nav_document.is_navigation)
-        self.assertEqual(package.navigation_doc, expected_nav_document)
+        self.assertEqual(package.navigation, expected_nav_document)
 
         # Verify reference to a resource item.
         # This also checks iteration and containment
         resource = [i for i in package
                     if i.name == "e3d625fe893b3f1f9aaef3bdf6bfa15c.png"][0]
         self.assertIn(resource, package)
+
+
+class HtmlMetadataParsingTestCase(unittest.TestCase):
+
+    def test_uid_ordering(self):
+        from cnxepub import _parse_document_metadata as parser
+
+        filepath = os.path.join(TEST_DATA_DIR, 'ordered-uids.xhtml')
+        with open(filepath, 'r') as fb:
+            doc = etree.parse(fb)
+        metadata = parser(doc)
+
+        expected =  ['Author 3', 'Author 12', 'Author 4',
+                     'Author 1', 'Author 10']
+        self.assertEqual(metadata['authors'], expected)
+
+
+class HtmlNavigationParsingTestCase(unittest.TestCase):
+
+    def test_parsing(self):
+        from cnxepub import Item, _parse_epub_navigation
+
+        filepath = os.path.join(TEST_DATA_DIR, 'nav-tree.xhtml')
+        with open(filepath) as fb:
+            item = Item('no-name', fb)
+            docs, tree, metadata = _parse_epub_navigation(item)
+
+        expected_docs = ['e78d4f90-e078-49d2-beac-e95e8be70667@3.xhtml',
+                         '3c448dc6-d5f5-43d5-8df7-fe27d462bd3a@1.xhtml',
+                         'ad17c39c-d606-4441-b987-54448020bb40@2.xhtml',
+                         '7c52af05-05b1-4761-aa4c-b17b0197dc6d@1.xhtml',
+                         ]
+        expected_tree = {
+            'id': 'no-name',
+            'contents': [
+                {'id': 'subcol',
+                 'contents': [
+                     {'id': 'subcol',
+                      'contents': [
+                          {'id': 'e78d4f90-e078-49d2-beac-e95e8be70667@3.xhtml',
+                           'title': 'Document One'}],
+                      'title': 'Chapter One'},
+                     {'id': 'subcol',
+                      'contents': [
+                          {'id': '3c448dc6-d5f5-43d5-8df7-fe27d462bd3a@1.xhtml',
+                           'title': 'Document Two'}],
+                      'title': 'Chapter Two'}],
+                 'title': 'Part One'},
+                {'id': 'subcol',
+                 'contents': [
+                    {'id': 'subcol',
+                     'contents': [
+                         {'id': 'ad17c39c-d606-4441-b987-54448020bb40@2.xhtml',
+                          'title': 'Document Three'}],
+                     'title': 'Chapter Three'}],
+                 'title': 'Part Two'},
+                {'id': 'subcol',
+                 'contents': [
+                     {'id': 'subcol',
+                      'contents': [
+                          {'id': '7c52af05-05b1-4761-aa4c-b17b0197dc6d@1.xhtml',
+                           'title': 'Document Four'}],
+                      'title': 'Chapter Four'}],
+                 'title': 'Part Three'}],
+            'title': 'Book One'}
+
+        self.assertEqual(docs, expected_docs)
+        self.assertEqual(tree, expected_tree)
+
+class EPubParsingTestCase(unittest.TestCase):
+    # This only checks the adaptation, not the archival the content.
+    #   Minimal database interaction is required
+
+    def test_epub(self):
+        # Parses a collection oriented epub content to file mappings.
+        from cnxepub import EPUB
+        epub = EPUB.from_file(TEST_EPUB_FILEPATH)
+
+        # Chop the epub apart.
+        from cnxepub import epub_to_mapping
+        mapping = epub_to_mapping(epub)
+
+        expected_package_mapping = {
+            # '{filename}': [{document-data-key}, ...],
+            'e78d4f90-e078-49d2-beac-e95e8be70667@3.xhtml': ['document', 'type',
+                                                             'metadata'],
+            '9b0903d2-13c4-4ebe-9ffe-1ee79db28482@1.6.xhtml': ['tree', 'type',
+                                                               'metadata'],
+            '3c448dc6-d5f5-43d5-8df7-fe27d462bd3a@1.xhtml': ['document', 'type',
+                                                             'metadata'],
+            'ad17c39c-d606-4441-b987-54448020bb40@2.xhtml': ['document', 'type',
+                                                             'metadata'],
+            '7c52af05-05b1-4761-aa4c-b17b0197dc6d@1.xhtml': ['document', 'type',
+                                                             'metadata'],
+            }
+
+        dense_mapping = {k:v.keys() for k, v in mapping[0].items()}
+        self.assertEqual(dense_mapping, expected_package_mapping)
+
+    @unittest.skip("not implemented, waiting to see if the base cases "
+                   "(formatting and values) are acceptable.")
+    def test_collection_w_extras(self):
+        # Parse an epub containing content that is not referenced within the
+        #   navigation or documents.
+        from cnxepub import EPUB
+        epub = EPUB.from_file(TEST_EPUB_W_EXTRAS_FILEPATH)
+
+        # Check that it dropped the extra content.
