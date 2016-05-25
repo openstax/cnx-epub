e6a8bd61ed82a1de862d8eadfdb31dda5577b1a8
Michael Mulich <michael.mulich@gmail.com>
2016-05-25 11:23:34 -0700

    Add a test for the validate-collated script

---
+++ cnxepub/tests/scripts/test_collated_single_html.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2016, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+
+import mimetypes
+import os.path
+import tempfile
+import unittest
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from lxml import etree
+
+from ...html_parsers import HTML_DOCUMENT_NAMESPACES
+from ...testing import TEST_DATA_DIR, captured_output
+
+
+class CollatedSingleHTMLTestCase(unittest.TestCase):
+    maxDiff = None
+
+    @property
+    def target(self):
+        from ...scripts.collated_single_html.main import main
+        return main
+
+    @property
+    def path_to_html(self):
+        return os.path.join(TEST_DATA_DIR, 'desserts-single-page.html')
+
+    def test_valid(self):
+        return_code = self.target([self.path_to_html])
+        self.assertEqual(return_code, 0)
