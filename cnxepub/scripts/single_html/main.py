ef233037741c4521cdf2a019a371959ace94c7b2
Ross Reedstrom <reedstrm@gmail.com>
2016-11-01 11:54:39 -0500

    Add cli script option for math conversion
    make math replacement more robust to empty return
    add logging

--- cnxepub/scripts/single_html/main.py
+++ cnxepub/scripts/single_html/main.py
@@ -23,7 +23,7 @@ MATHJAX_URL = 'https://cdn.mathjax.org/mathjax/{mathjax_version}/'\
 
 parts = ['page', 'chapter', 'unit', 'book', 'series']
 partcount = {}
-logger = logging.getLogger('single_html')
+logger = logging.getLogger('cnxepub')
 
 
 def single_html(epub_file_path, html_out=sys.stdout, mathjax_version=None,
@@ -92,6 +92,10 @@ def main(argv=None):
     parser.add_argument('-t', "--exercise_token",
                         metavar="exercise_token", nargs="?",
                         help="Token for including answers in exercises")
+    parser.add_argument('-M', "--mathmlcloud_url",
+                        metavar="mathmlcloud_url", nargs="?",
+                        help="Convert TeX equations using "
+                             "this mathmlcloud API url")
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Send debugging info to stderr')
     parser.add_argument('-s', '--subset-chapters', dest='numchapters',
@@ -113,12 +117,15 @@ def main(argv=None):
 
     exercise_host = args.exercise_host
     exercise_token = args.exercise_token
+    mml_url = args.mathmlcloud_url
     if exercise_host:
         exercise_url = \
                 'https://%s/api/exercises?q=tag:{itemCode}' % (exercise_host)
         exercise_match = '#ost/api/ex/'
         includes = [exercise_callback_factory(exercise_match,
-                                              exercise_url, exercise_token)]
+                                              exercise_url,
+                                              exercise_token,
+                                              mml_url)]
     else:
         includes = None
 
