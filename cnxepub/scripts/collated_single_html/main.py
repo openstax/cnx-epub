c0b08a6dd5674c87d54a85cce650783613440fcc
Ross Reedstrom <reedstrm@gmail.com>
2016-12-14 14:03:56 -0600

    add option to copy resources from input epub

--- cnxepub/scripts/collated_single_html/main.py
+++ cnxepub/scripts/collated_single_html/main.py
@@ -14,8 +14,8 @@ import argparse
 import logging
 import sys
 from pprint import pformat
-from lxml import etree
 import cnxepub
+from zipfile import ZipFile, ZIP_DEFLATED
 
 logger = logging.getLogger('cnxepub')
 
@@ -34,8 +34,11 @@ def main(argv=None):
     parser.add_argument('-d', '--dump-tree', action='store_true',
                         help='Print out parsed model tree.')
 
-    parser.add_argument('-o', '--output', type=argparse.FileType('w'),
+    parser.add_argument('-o', '--output', type=argparse.FileType('w+'),
                         help='Write out epub of parsed tree.')
+
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'),
+                        help='Read and copy resources/ for output epub.')
 
     args = parser.parse_args(argv)
 
@@ -48,6 +51,17 @@ def main(argv=None):
 
     if args.output:
         cnxepub.adapters.make_epub(binder, args.output)
+
+    if args.input:
+        args.output.seek(0)
+        zout = ZipFile(args.output, 'a', ZIP_DEFLATED)
+        zin = ZipFile(args.input, 'r')
+        for res in zin.namelist():
+            if res.startswith('resources'):
+                zres = zin.open(res)
+                zi = zin.getinfo(res)
+                zout.writestr(zi, zres.read(), ZIP_DEFLATED)
+        zout.close()
 
     # TODO Check for documents that have no identifier.
     #      These should likely be composite-documents
