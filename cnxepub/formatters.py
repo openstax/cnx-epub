5960082ba157e5b16b75721bf4eb28760f5ff55d
Kendra Swanson <kendraswan@gmail.com>
2021-08-26 15:50:22 -0400

    linter fixes

--- cnxepub/formatters.py
+++ cnxepub/formatters.py
@@ -596,6 +596,7 @@ def render_exercise(exercise):
 
     return template.render(data=exercise_content)
 
+
 DOCUMENT_POINTER_TEMPLATE = """\
 <?xml version="1.0" encoding="UTF-8"?>
 <html xmlns="http://www.w3.org/1999/xhtml"
